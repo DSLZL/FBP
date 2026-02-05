723f680be45a717357847648c7c4b0d07eadf6f1
DSLZL <q2891362919@163.com>
2026-02-05 15:15:01 +0800

    init

--- data.lua
+++ data.lua
@@ -5,15 +5,10 @@ data:extend({
     order = "a",
     action = "lua",
     toggleable = true,
-    -- 自定义图标示例 (取消注释以使用):
-    -- icon = "__Factorio_Blueprint_Printer__/graphics/icons/icon_placement.png",
-    -- icon_size = 64,
-    icon = "__base__/graphics/icons/construction-robot.png",
+    icon = "__Factorio_Blueprint_Printer__/graphics/icons/icon_placement.png",
     icon_size = 64,
     icon_mipmaps = 4,
-    -- small_icon = "__Factorio_Blueprint_Printer__/graphics/icons/icon_placement.png",
-    -- small_icon_size = 64,
-    small_icon = "__base__/graphics/icons/construction-robot.png",
+    small_icon = "__Factorio_Blueprint_Printer__/graphics/icons/icon_placement.png",
     small_icon_size = 64,
     small_icon_mipmaps = 4
   },
@@ -23,15 +18,10 @@ data:extend({
     order = "b",
     action = "lua",
     toggleable = true,
-    -- 自定义图标示例 (取消注释以使用):
-    -- icon = "__Factorio_Blueprint_Printer__/graphics/icons/icon_deconstruction.png",
-    -- icon_size = 64,
-    icon = "__base__/graphics/icons/deconstruction-planner.png",
+    icon = "__Factorio_Blueprint_Printer__/graphics/icons/icon_deconstruction.png",
     icon_size = 64,
     icon_mipmaps = 4,
-    -- small_icon = "__Factorio_Blueprint_Printer__/graphics/icons/icon_deconstruction.png",
-    -- small_icon_size = 64,
-    small_icon = "__base__/graphics/icons/deconstruction-planner.png",
+    small_icon = "__Factorio_Blueprint_Printer__/graphics/icons/icon_deconstruction.png",
     small_icon_size = 64,
     small_icon_mipmaps = 4
   }
